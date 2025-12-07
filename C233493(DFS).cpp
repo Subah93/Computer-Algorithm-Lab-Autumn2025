5e6fdb904c42eb990a3ab2f6f63b7192b6e47e3a
Subah93 <omarasubah@gmail.com>
2025-12-08 00:44:38 +0600

    Add files via upload

---
+++ C233493(DFS).cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> nodeNeighbour[100002];
+bool isVisited[100002];
+
+int main()
+{
+    int nodes;
+    cin >> nodes;
+    int edges;
+    cin >> edges;
+    for(int i = 0; i < nodes; i++)
+    {
+        isVisited[i + 1] = false;
+    }
+    for(int i = 0; i < edges; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        nodeNeighbour[u].push_back(v);
+        nodeNeighbour[v].push_back(u);
+    }
+    int startNode;
+    cin >> startNode;
+    stack<int> s;
+    s.push(startNode);
+    while(!s.empty())
+    {
+    	int popped = s.top();
+    	isVisited[popped] = true;
+    	s.pop();
+    	for(int n: nodeNeighbour[popped])
+    	{
+    		if(!isVisited[n])
+    			s.push(n);
+    	}
+    }
+    int counter = 0;
+    for(int i = 1; i <= nodes; i++) {
+    	if(!isVisited[i])
+    		counter++;
+    }
+    cout << counter << endl;
+    return 0;
+}
